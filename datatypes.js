0a464b58c844b15bd802e732037675dcef046ead
ihechilurub <168141705+ihechilurub@users.noreply.github.com>
2024-05-13 16:10:06 +0000

    Create datatypes.js

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// data type number
+// let number = 10;
+// let number2 = 20;
+// let number3 = 30;
+
+// how to wrte a function
+// function addNumbers(number, number){
+
+ //   console.log(number + number2);
+// }
+
+//addNumbers(number,number2);
+
+// STRING DATATYPE
+// let names = "DRAPE"
+
+// console.log(names)
+
+
+// Bolean Datatypes
+ let itvalid = false
+
+ console.log(isvalid);
