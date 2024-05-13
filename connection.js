f5307bd3a742c4d2e139fd1cee7548f56db209fb
ihechilurub <168141705+ihechilurub@users.noreply.github.com>
2024-05-13 16:16:39 +0000

    Create connection.js

---
+++ connection.js
@@ -0,0 +1,9 @@
+const mysql = require('mysql');
+
+// Create a connection
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'blessing',
+    password: 'bless247',
+    database: 'ihechi'
+  });
