01fb0edcc9a0705d68342aa11912f6487040825b
ihechilurub <168141705+ihechilurub@users.noreply.github.com>
2024-05-13 16:28:40 +0000

    Create student.js

---
+++ student.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const student = {
+    name: bless,
+    email: bless,
+    email: bless,
+    date: { type: 4/2/2006 }
+}
+const schema = new mongoose.Schema(studentsModel);
+
+const studentSchema = mongoose.model('students', schema);
+
+module.exports =  studentSchema;
